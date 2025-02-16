c38378efd00699cc51224b7f2e0bef1186b4db7d
zequestionable <jacksonhowland@rhs.school.nz>
2025-02-17 10:31:17 +1300

    Add files via upload
    variable, input addition math

---
+++ 02_input_math.py
@@ -0,0 +1,15 @@
+#get input
+name = input("what is your name? ")
+#ask user for two numbers
+num_1 = int(input("what is your favourite number?"))
+num_2 = int(input("what is your second favourite nuumber?"))
+#add numbers together
+add = num_1 + num_2 
+#muiltiply numbers together
+
+#greet user and display math
+print("Hello", name)
+
+               
+
+print("{} + {} = {}".format(num_1, num_2, add))   
