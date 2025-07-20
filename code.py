b69adafdf1db9744e894ff159e780eb5a88cbfcc
zequestionable <jacksonhowland@rhs.school.nz>
2025-07-21 10:57:35 +1200

    Add files via upload
    This is my first start. mon 21 Jul

---
+++ code.py
@@ -0,0 +1,11 @@
+import board
+import digitalio
+import time
+
+led0 = digitalio.DigitalInOut(board.GP0)
+led0.direction = digitalio.Direction.OUTPUT
+
+push = input("push this button to start the game")
+
+  if push = True:
+
